098f357f9267ce46c31e452bb3f566414fc2a3ba
Mukesh Kumar <mukeshvrt.096@gmail.com>
2026-08-27 17:15:33 +0530

    Update testing4.py

--- testing4.py
+++ testing4.py
@@ -1,7 +1,4 @@
 print("Hello Dosto , How are you?")
 print("Today, I have learnt Python")
-<<<<<<< HEAD
 print("Learning Python is very challenging")
-=======
 print("How's your day?")
->>>>>>> 12f2d57c212cf12e4c6c27168b7a2409ffb6dce2
